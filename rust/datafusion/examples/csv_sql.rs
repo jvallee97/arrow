696b16bcccac1b2085e4fc4c6e71fb8aa6d082a9
jvallee <jasonmvallee@gmail.com>
2020-07-12 21:49:01 -0500

    cpu monitoring and average for 10 aggregate hash's

--- rust/datafusion/examples/csv_sql.rs
+++ rust/datafusion/examples/csv_sql.rs
@@ -14,9 +14,10 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+extern crate cpu_monitor;
 
 use arrow::util::pretty;
-use systemstat::{System, Platform, saturating_sub_bytes};
+//use systemstat::{System, Platform, saturating_sub_bytes};
 use std::thread;
 use std::time::Duration;
 
@@ -24,47 +25,70 @@ use datafusion::datasource::csv::CsvReadOptions;
 use datafusion::error::Result;
 use datafusion::execution::context::ExecutionContext;
 
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
+use cpu_monitor::CpuInstant;
 /// This example demonstrates executing a simple query against an Arrow data source (CSV) and
 /// fetching results
 fn main() -> Result<()> {
     // create local execution context
-    let mut ctx = ExecutionContext::new();
-    print!("here before arrow\n\n\n");
-    let sys = System::new();
-    match sys.cpu_load_aggregate() {
-        Ok(cpu)=> {
-            println!("\nMeasuring CPU load...");
-            thread::sleep(Duration::from_secs(1));
-            let cpu = cpu.done().unwrap();
-            println!("CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
-                     cpu.user * 100.0, cpu.nice * 100.0, cpu.system * 100.0, cpu.interrupt * 100.0, cpu.idle * 100.0);
-        },
-        Err(x) => println!("\nCPU load: error: {}", x)
+    let mut x = 0;
+    let temp = CpuInstant::now()?;
+    let mut avg = 0.0;
+
+
+    while x < 10
+    {
+        x+=1;
+        let mut ctx = ExecutionContext::new();
+        let mut sysinfo = System::new_all();
+
+        // Number of processors
+        println!("NB processors: {}", sysinfo.get_processors().len());
+        //let testdata = std::env::var("ARROW_TEST_DATA").expect("ARROW_TEST_DATA not Defined");
+        //let testdata = "/Users/jasonmichaelvallee/CS/data/Arrow/testing/data";
+        let testdata = "../../testing/data";
+        let start = CpuInstant::now()?;
+
+        // register csv file with the execution context
+        ctx.register_csv(
+            "aggregate_test_100",
+            &format!("{}/csv/aggregate_test_100.csv", testdata),
+            CsvReadOptions::new(),
+        )?;
+
+        let sql = "SELECT c1, MIN(c12), MAX(c12) FROM aggregate_test_100 WHERE c11 > 0.1 AND c11 < 0.9 GROUP BY c1";
+
+        // create the query plan
+        let plan = ctx.create_logical_plan(sql)?;
+        let plan = ctx.optimize(&plan)?;
+        let plan = ctx.create_physical_plan(&plan, 1024 * 1024)?;
+
+        // execute the query
+        let results = ctx.collect(plan.as_ref())?;
+        // Number of processors
+        // print the results
+        //pretty::print_batches(&results)?;
+        let end = CpuInstant::now()?;
+
+        let duration = end - start;
+        println!("PREV AVG: {}%", avg);
+        println!("CPU usage for Query: {}%", duration.non_idle());
+
+        avg += duration.non_idle();
+        println!("AVG: {}%", avg);
+
+        println!("CPU usage for Query: {}%", duration.non_idle());
+        println!("CPU usage for Query: {:.0}%", duration.non_idle()*100.);
+
+
+        let start = CpuInstant::now()?;
+        std::thread::sleep(Duration::from_millis(100));
+        let end = CpuInstant::now()?;
+        let duration = end - start;
+
+        println!("CPU usage for sleep thread: {:.0}%", duration.non_idle() * 100.);
     }
-    //let testdata = std::env::var("ARROW_TEST_DATA").expect("ARROW_TEST_DATA not Defined");
-    //let testdata = "/Users/jasonmichaelvallee/CS/data/Arrow/testing/data";
-    let testdata = "../../testing/data";
 
-
-    // register csv file with the execution context
-    ctx.register_csv(
-        "aggregate_test_100",
-        &format!("{}/csv/aggregate_test_100.csv", testdata),
-        CsvReadOptions::new(),
-    )?;
-
-    let sql = "SELECT c1, MIN(c12), MAX(c12) FROM aggregate_test_100 WHERE c11 > 0.1 AND c11 < 0.9 GROUP BY c1";
-
-    // create the query plan
-    let plan = ctx.create_logical_plan(sql)?;
-    let plan = ctx.optimize(&plan)?;
-    let plan = ctx.create_physical_plan(&plan, 1024 * 1024)?;
-
-    // execute the query
-    let results = ctx.collect(plan.as_ref())?;
-
-    // print the results
-    pretty::print_batches(&results)?;
-
+    println!("\n\nCPU Average usage: {:.0}%", avg * 10.);
     Ok(())
 }
